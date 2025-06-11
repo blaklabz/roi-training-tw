d0ad8e278c9cdbff459207a0df111d5ce98b41ea
blaklabz <blaklabz@blaklabz.local>
2025-06-11 16:53:55 -0400

    creating infrastructure-pipelines

--- 03-devops-capstone/infra-pipelines/eks/terraform/main.tf
+++ 03-devops-capstone/infra-pipelines/eks/terraform/main.tf
@@ -56,20 +56,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
-# Outputs
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
-
-output "node_group_role_arn" {
-  value = module.eks.eks_managed_node_groups["spot_node_group"].iam_role_arn
-}
--- 03-devops-capstone/infra-pipelines/eks/terraform/outputs.tf
+++ 03-devops-capstone/infra-pipelines/eks/terraform/outputs.tf
@@ -9,3 +9,7 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
+
+output "node_group_role_arn" {
+  value = module.eks.eks_managed_node_groups["spot_node_group"].iam_role_arn
+}
