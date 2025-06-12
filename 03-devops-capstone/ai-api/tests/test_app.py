b6fed7a005489a74bd6e753169ee226af97dd0e8
blaklabz <blaklabz@blaklabz.local>
2025-06-12 12:38:32 -0400

    updating-pytest

--- 03-devops-capstone/ai-api/tests/test_app.py
+++ 03-devops-capstone/ai-api/tests/test_app.py
@@ -1,6 +1,12 @@
 import pytest
 from app import app
 from unittest.mock import patch
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app
 
 @pytest.fixture
 def client():
